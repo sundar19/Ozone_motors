32173263844a4116a6c0e4b1be1c1de256f4c177
sundar <krisha.sundar19@gmail.com>
2021-03-16 12:10:31 +0530

    mul_imu

---
+++ multiple_mpu6050_xiao/xiao_imu/xiao_imu.ino
@@ -0,0 +1,155 @@
+#include<Wire.h>
+unsigned long mytime;
+float time1 , time2 , time3;
+const int MPU6050_addr=0x68;
+int16_t AccX,AccY,AccZ,Temp,GyroX,GyroY,GyroZ;
+void setup(){
+  pinMode(0,OUTPUT);
+  pinMode(1,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  Wire.begin();
+  Serial.begin(115200);
+}
+void loop(){
+  
+ imu1();
+ 
+ imu2();
+
+ imu3();
+
+ imu4();
+}
+void imu1()
+{
+   digitalWrite(0,LOW);// 1st MPU6050
+   Wire.beginTransmission(MPU6050_addr);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+  Wire.beginTransmission(MPU6050_addr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU6050_addr,14,true);
+  //time1 = micros();
+  AccX=Wire.read()<<8|Wire.read();
+  AccY=Wire.read()<<8|Wire.read();
+  AccZ=Wire.read()<<8|Wire.read();
+  //Temp=Wire.read()<<8|Wire.read();
+  GyroX=Wire.read()<<8|Wire.read();
+  GyroY=Wire.read()<<8|Wire.read();
+  GyroZ=Wire.read()<<8|Wire.read();
+  //time2 = micros();
+  //time3 = time2-time1;
+  //Serial.println(time3);
+  Serial.println("data from 1st imu");
+  Serial.print("AccX = "); Serial.print(AccX);
+  Serial.print(" || AccY = "); Serial.print(AccY);
+  Serial.print(" || AccZ = "); Serial.print(AccZ);
+  //Serial.print(" || Temp = "); Serial.print(Temp/340.00+36.53);
+  Serial.print(" || GyroX = "); Serial.print(GyroX);
+  Serial.print(" || GyroY = "); Serial.print(GyroY);
+  Serial.print(" || GyroZ = "); Serial.println(GyroZ);
+  delay(100);
+  digitalWrite(0,HIGH);
+}
+void imu2()
+{
+  digitalWrite(1,LOW);// 2nd MPU6050
+  Wire.beginTransmission(MPU6050_addr);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+  Wire.beginTransmission(MPU6050_addr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU6050_addr,14,true);
+  //time1 = micros();
+  AccX=Wire.read()<<8|Wire.read();
+  AccY=Wire.read()<<8|Wire.read();
+  AccZ=Wire.read()<<8|Wire.read();
+  //Temp=Wire.read()<<8|Wire.read();
+  GyroX=Wire.read()<<8|Wire.read();
+  GyroY=Wire.read()<<8|Wire.read();
+  GyroZ=Wire.read()<<8|Wire.read();
+  //time2 = micros();
+  //time3 = time2-time1;
+  //Serial.println(time3);
+  Serial.println("data from 2nd imu");
+  Serial.print("AccX = "); Serial.print(AccX);
+  Serial.print(" || AccY = "); Serial.print(AccY);
+  Serial.print(" || AccZ = "); Serial.print(AccZ);
+  //Serial.print(" || Temp = "); Serial.print(Temp/340.00+36.53);
+  Serial.print(" || GyroX = "); Serial.print(GyroX);
+  Serial.print(" || GyroY = "); Serial.print(GyroY);
+  Serial.print(" || GyroZ = "); Serial.println(GyroZ);
+  delay(100);
+  digitalWrite(1,HIGH);
+}
+void imu3()
+{
+  digitalWrite(2,LOW);// 3rd MPU6050
+  Wire.beginTransmission(MPU6050_addr);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+  Wire.beginTransmission(MPU6050_addr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU6050_addr,14,true);
+  //time1 = micros();
+  AccX=Wire.read()<<8|Wire.read();
+  AccY=Wire.read()<<8|Wire.read();
+  AccZ=Wire.read()<<8|Wire.read();
+  //Temp=Wire.read()<<8|Wire.read();
+  GyroX=Wire.read()<<8|Wire.read();
+  GyroY=Wire.read()<<8|Wire.read();
+  GyroZ=Wire.read()<<8|Wire.read();
+  //time2 = micros();
+  //time3 = time2-time1;
+  //Serial.println(time3);
+  Serial.println("data from 3rd imu");
+  Serial.print("AccX = "); Serial.print(AccX);
+  Serial.print(" || AccY = "); Serial.print(AccY);
+  Serial.print(" || AccZ = "); Serial.print(AccZ);
+  //Serial.print(" || Temp = "); Serial.print(Temp/340.00+36.53);
+  Serial.print(" || GyroX = "); Serial.print(GyroX);
+  Serial.print(" || GyroY = "); Serial.print(GyroY);
+  Serial.print(" || GyroZ = "); Serial.println(GyroZ);
+  delay(100);
+  digitalWrite(2,HIGH);
+}
+void imu4()
+{
+  digitalWrite(3,LOW);// 4th MPU6050
+  Wire.beginTransmission(MPU6050_addr);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+  Wire.beginTransmission(MPU6050_addr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU6050_addr,14,true);
+  //time1 = micros();
+  AccX=Wire.read()<<8|Wire.read();
+  AccY=Wire.read()<<8|Wire.read();
+  AccZ=Wire.read()<<8|Wire.read();
+  //Temp=Wire.read()<<8|Wire.read();
+  GyroX=Wire.read()<<8|Wire.read();
+  GyroY=Wire.read()<<8|Wire.read();
+  GyroZ=Wire.read()<<8|Wire.read();
+  //time2 = micros();
+  //time3 = time2-time1;
+  //Serial.println(time3);
+  Serial.println("data from 4th imu");
+  Serial.print("AccX = "); Serial.print(AccX);
+  Serial.print(" || AccY = "); Serial.print(AccY);
+  Serial.print(" || AccZ = "); Serial.print(AccZ);
+  //Serial.print(" || Temp = "); Serial.print(Temp/340.00+36.53);
+  Serial.print(" || GyroX = "); Serial.print(GyroX);
+  Serial.print(" || GyroY = "); Serial.print(GyroY);
+  Serial.print(" || GyroZ = "); Serial.println(GyroZ);
+  delay(100);
+  digitalWrite(3,HIGH);
+}
